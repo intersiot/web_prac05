2dd5e8e9014ff5e479698f36517b55af64c6a6cd
intersiot <75784873+intersiot@users.noreply.github.com>
2020-12-31 20:06:32 +0900

    Update app.py

--- prac/app.py
+++ prac/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 from pymongo import MongoClient
 
-client = MongoClient('mongodb://3.35.47.80', 27017, username="test", password="test")
+client = MongoClient('mongodb://내AWS아이피', 27017, username="아이디", password="")
 db = client.dbsparta_plus_week4
 
 # JWT 토큰을 만들 때 필요한 비밀문자열입니다. 아무거나 입력해도 괜찮습니다.
